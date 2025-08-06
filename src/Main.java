612d3b50f365756b195cba5e5d0f18c709c1aff3
SaltedFries <tranblueneverdie@gmail.com>
2025-08-06 16:12:11 -0700

    as

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to MindDump!");
+        System.out.println("What do you wanna do today? ");
+
+        while (true){
+            System.out.println("[1] Write Entry");
+            System.out.println("[2] View Entries");
+            System.out.println("[3] View All");
+            System.out.println("[4] Exit");
+            System.out.print("> ");
+
+            int input = scanner.nextInt();
+            String s = scanner.nextLine();
+
+            switch (input){
+                case 1 -> {
+                    System.out.println("Write entry here: ");
+                }
+                case 2 -> {
+                    System.out.println("View entries here: ");
+                }
+                case 3 -> {
+                    System.out.println("View all entries: ");
+                }
+                case 4 -> {
+                    System.out.println("Goodbye, have a good day!");
+                    break;
+                }
+            }
+        }
+
     }
 }
